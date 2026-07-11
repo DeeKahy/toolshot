b20a82aed9e47c471aaa7cdce70981fa4cc38cc1
DeeKahy <97156953+DeeKahy@users.noreply.github.com>
2026-07-11 18:03:45 +0200

    Release v0.3.0

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
         default = toolshot;
         toolshot = pkgs.rustPlatform.buildRustPackage {
           pname = "toolshot";
-          version = "0.2.3";
+          version = "0.3.0";
           src = ./.;
           # Workspace build: the toolshot daemon and the toolshot-ui
           # Tauri app, both installed side by side.
@@ -53,8 +53,8 @@
               <key>CFBundleDisplayName</key><string>Toolshot</string>
               <key>CFBundleIdentifier</key><string>dev.deekahy.toolshot</string>
               <key>CFBundleExecutable</key><string>toolshot</string>
-              <key>CFBundleVersion</key><string>0.2.3</string>
-              <key>CFBundleShortVersionString</key><string>0.2.3</string>
+              <key>CFBundleVersion</key><string>0.3.0</string>
+              <key>CFBundleShortVersionString</key><string>0.3.0</string>
               <key>CFBundleIconFile</key><string>icon.icns</string>
               <key>LSMinimumSystemVersion</key><string>11.0</string>
               <key>LSUIElement</key><true/>
